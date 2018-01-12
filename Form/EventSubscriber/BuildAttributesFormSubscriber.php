f50194c554105aca7dcad5c9cfc46a7109615874
David <david_courtey@yahoo.com>
2018-01-12 18:55:57 +0100

    removing removeAttrivute if null because causing an different in attributes from different lang

--- Form/EventSubscriber/BuildAttributesFormSubscriber.php
+++ Form/EventSubscriber/BuildAttributesFormSubscriber.php
@@ -102,7 +102,7 @@ final class BuildAttributesFormSubscriber implements EventSubscriberInterface
         /** @var AttributeValueInterface $attribute */
         foreach ($product->getAttributes() as $attribute) {
             if (null === $attribute->getValue()) {
-                $product->removeAttribute($attribute);
+                //$product->removeAttribute($attribute);
             }
         }
     }
